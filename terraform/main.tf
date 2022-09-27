52c6d2499ee1657737c044ce3fa3a7cc92b93d73
implodingduck <implodingduck@users.noreply.github.com>
2022-09-27 10:39:40 -0500

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.21.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "=3.1.0"
+    }
+  }
+  backend "azurerm" {
+
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+}
+
+locals {
+  func_name = "func${random_string.unique.result}"
+  loc_for_naming = lower(replace(var.location, " ", ""))
+  gh_repo = replace(var.gh_repo, "implodingduck/", "")
+  tags = {
+    "managed_by" = "terraform"
+    "repo"       = local.gh_repo
+  }
+}
+
+resource "random_string" "unique" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_log_analytics_workspace" "default" {
+  name                = "DefaultWorkspace-${data.azurerm_client_config.current.subscription_id}-EUS"
+  resource_group_name = "DefaultResourceGroup-EUS"
+} 
+
+data "azurerm_network_security_group" "basic" {
+    name                = "basic"
+    resource_group_name = "rg-network-eastus"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${local.gh_repo}-${random_string.unique.result}-${local.loc_for_naming}"
+  location = var.location
+  tags = local.tags
+}
