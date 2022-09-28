115f80f3452a5b945e24a80783644ff89ed3cab6
implodingduck <zsek320@gmail.com>
2022-09-28 10:51:23 -0500

    more updates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# base-repo
+# AKS Workload Identity
+
+* Create an AKS cluster behind HTTP proxy and generated a managed identity with a federated identity credential.
+* Kubernetes yaml to use the workload identity https://azure.github.io/azure-workload-identity/docs/quick-start.html
--- terraform/main.tf
+++ terraform/main.tf
@@ -250,3 +250,72 @@ resource "azapi_resource" "fic" {
     }
   })
 }
+
+resource "azurerm_key_vault" "kv" {
+  name                       = "kv-${local.func_name}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+  
+}
+
+resource "azurerm_key_vault_access_policy" "sp" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+  
+  key_permissions = [
+    "Create",
+    "Get",
+    "Purge",
+    "Recover",
+    "Delete"
+  ]
+
+  secret_permissions = [
+    "Set",
+    "Purge",
+    "Get",
+    "List",
+    "Delete"
+  ]
+
+  certificate_permissions = [
+    "Purge"
+  ]
+
+  storage_permissions = [
+    "Purge"
+  ]
+  
+}
+
+
+resource "azurerm_key_vault_access_policy" "uai" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = azurerm_user_assigned_identity.fic.principal_id
+  
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+    "List"
+  ]
+  
+}
+
+resource "azurerm_key_vault_secret" "helloworld" {
+  depends_on = [
+    azurerm_key_vault_access_policy.sp
+  ]
+  name         = "hello"
+  value        = "world"
+  key_vault_id = azurerm_key_vault.kv.id
+  tags         = local.tags
+}
